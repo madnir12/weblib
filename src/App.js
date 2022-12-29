fb6e2f1de89d1282a4ba420c2c12844809f8e702
Haq Nawaz <haq.hawaz@hiddenlogics.com>
2022-12-29 21:01:49 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import $ from "jquery";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [html, setHtml] = useState("");
+  function wrapText(elementID, openTag, closeTag) {
+    var textArea = $("#" + elementID);
+    var len = textArea.val().length;
+    var start = textArea[0].selectionStart;
+    var end = textArea[0].selectionEnd;
+    var selectedText = textArea.val().substring(start, end);
+    var replacement = openTag + selectedText + closeTag;
+    if (end !== start) {
+      textArea.val(
+        textArea.val().substring(0, start) +
+          replacement +
+          textArea.val().substring(end, len)
+      );
+      setHtml(textArea.val());
+    }
+  }
+
+  function onContextMenu(event) {
+    event.preventDefault();
+    var menu = $("#contextMenu");
+    menu.css({
+      top: event.pageY + "px",
+      left: event.pageX + "px",
+    });
+    menu.show();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        className="rtl"
+        id="myTa"
+        onContextMenu={onContextMenu}
+      ></textarea>
+
+      <button
+        id="underline"
+        onClick={() =>
+          wrapText("myTa", "<span class='quran-marker' >", "</span>")
+        }
+      >
+        قرآن
+      </button>
+
+      <button
+        id="underline"
+        onClick={() =>
+          wrapText("myTa", "<span class='hadees-marker' >", "</span>")
+        }
+      >
+        حدیث
+      </button>
+
+      <button
+        id="bold"
+        onClick={() =>
+          wrapText("myTa", "<span class='urdu-marker' >", "</span>")
+        }
+      >
+        اردو
+      </button>
+      <button id="italic" onClick={() => wrapText("myTa", "<em>", "</em>")}>
+        i
+      </button>
+
+      <button
+        id="underline"
+        onClick={() =>
+          wrapText("myTa", "<span class='ref-arabic-marker ' >", "</span>")
+        }
+      >
+        عربی حوالہ
+      </button>
+      <button
+        id="underline"
+        onClick={() =>
+          wrapText("myTa", "<span class='english-marker ' >", "</span>")
+        }
+      >
+        English
+      </button>
+      <button
+        id="code"
+        onClick={() => wrapText("myTa", "<strong>", "</strong>")}
+      >
+        {}
+      </button>
+      <button
+        id="underline"
+        onClick={() =>
+          wrapText("myTa", "<span class='heading1-marker ' >", "</span>")
+        }
+      >
+        ھیڈنگ 1
+      </button>
+      <div
+        className="rtl"
+        dangerouslySetInnerHTML={{
+          __html: html,
+        }}
+      ></div>
     </div>
   );
 }
-
-export default App;
